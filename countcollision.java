de6825e9f038aac7c2fb8d2d41392071cbcecbc5
Abdul Mosin <abdulmosin178@gmail.com>
2025-12-04 11:21:14 +0530

    new data added

---
+++ countcollision.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCollisions(String directions) {
+        int n = directions.length();
+        int i = 0, j = n - 1, ans = 0;
+        while (i < n && directions.charAt(i) == 'L') i++;
+        while (j >= 0 && directions.charAt(j) == 'R') j--;
+        for (int k = i; k <= j; k++)
+            if (directions.charAt(k) != 'S') ans++;
+        return ans;
+    }
+}
