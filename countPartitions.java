d5af7e28b14ed7944dbd37969e17cef0ed9d6f8f
Abdul Mosin <abdulmosin178@gmail.com>
2025-12-06 16:35:58 +0530

    soln addded

---
+++ countPartitions.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countPartitions(int[] nums) {
+        int total = 0;
+        for (int v : nums) total += v;
+        
+        // If total sum is even, all n-1 partitions are valid.
+        // If total sum is odd, no partition gives even difference.
+        return (total % 2 == 0) ? nums.length - 1 : 0;
+    }
+}
