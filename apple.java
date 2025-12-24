7402294ddc77b23ddd02ade353391497f7f19d2c
Abdul Mosin <abdulmosin178@gmail.com>
2025-12-24 10:50:25 +0530

    apple hey

---
+++ apple.java
@@ -0,0 +1,26 @@
+ import java.util.Arrays;
+
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int totalApples = 0;
+        for (int a : apple) {
+            totalApples += a;
+        }
+
+        
+        Arrays.sort(capacity);
+        
+        int currentCapacity = 0;
+        int boxes = 0;
+
+        for (int i = capacity.length - 1; i >= 0; i--) {
+            currentCapacity += capacity[i];
+            boxes++;
+            if (currentCapacity >= totalApples) {
+                return boxes;
+            }
+        }
+
+        return boxes; 
+    }
+}
